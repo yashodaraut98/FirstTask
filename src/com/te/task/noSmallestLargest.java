f1722c882fc667c7558de19cc3531a663d8bfa1f
yashodaraut98 <101801579+yashodaraut98@users.noreply.github.com>
2022-03-18 15:38:09 +0530

    Update noSmallestLargest.java

--- src/com/te/task/noSmallestLargest.java
+++ src/com/te/task/noSmallestLargest.java
@@ -4,8 +4,26 @@ public class noSmallestLargest {
 	public static void main(String args[])
 	{
 	int large,small,i;
-	int a[] = new int[]{1, 2, 3, 4, 5};
-	int n = a.length;
+int[] a= new int[] {78, 34, 1, 3, 90, 34, 6, 55, 20};  
+	System.out.println("Array elements after sorting:");  
+	//sorting logic  
+	for (int i1= 0; i1 < a.length; i1++)   
+	{  
+	for (int j = i1 + 1; j < a.length; j++)   
+	{  
+	int tmp = 0;  
+	if (a[i1] > a[j])   
+	{  
+	tmp = a[i1];  
+	a[i1] = a[j];  
+	a[j] = tmp;  
+	}  
+	}  
+	//prints the sorted element of the array  
+	System.out.println(a[i1]);  
+	}  
+	
+		int n = a.length;
 	large=small=a[0];
 	for(i=1;i<n;++i)
 	{
